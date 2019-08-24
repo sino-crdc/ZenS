f92b65f5c53ea3ab3f60a22b8e2e50a88fbb953d
ZenMoore <1057398161@qq.com>
2019-08-24 18:53:37 +0800

    创新点挖掘-手势新方式

---
+++ Gesture_Detect_Debug/train_class/train_class.ino
@@ -0,0 +1,14 @@
+/**
+ * train_class(训练分类)：
+ *    将所有数据读取并上传至网络(服务器)，经过与深度学习持久化模型的比对生成手势表达式或者角度表达式(甚至轨迹表达式)传回手环
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
